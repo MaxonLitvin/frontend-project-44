5d4b83ae1d4102bc837ea6e1be77651e8a4f8f53
MaxonLitvin <123316731+MaxonLitvin@users.noreply.github.com>
2023-05-26 14:13:24 +0300

    Step 7 (add gcd, brain-gcd.js)

---
+++ src/gcd.js
@@ -0,0 +1,29 @@
+import { RandomNubmers } from "./even-random-numbers.js";
+import runGameLogic, { roundNumbers } from "./gamelogic.js";
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const GetGcd = (number1, number2) => {
+    if (number1 === 0) {
+        return number2;
+    } 
+    return GetGcd(number2 % number1, number1);
+};
+
+const generationRound = () => {
+    const firstNumber = RandomNubmers(1, 100);
+    const secondNumber = RandomNubmers(1, 100);
+    const correctAnswer = String(GetGcd(firstNumber, secondNumber));
+    const question = `${firstNumber} ${secondNumber}`;
+    return [question, correctAnswer];
+};
+
+const runGcdGame = () => {
+    const round = [];
+    for (let i = 0; i < roundNumbers; i += 1) {
+        round[i] = generationRound();
+    }
+    return runGameLogic(round, description);
+};
+
+export default runGcdGame;
